9baf0a22fed64b6050a43fb98939a82e1cc43e47
Peter <yancong.li@alumni.ubc.ca>
2018-03-05 19:21:15 -0800

    Java Generics
    Generics with wildcards

--- more generics/src/client/Application.java
+++ more generics/src/client/Application.java
@@ -1,8 +1,6 @@
 package client;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class Application {
 	
@@ -39,6 +37,37 @@ public class Application {
 			System.out.println(iterator.next());
 		}
 		
+		Object myObject = new Object();
+		String myVar = "hello";
+		myObject = myVar;
+		
+		Employee emp = new Employee();
+		Accountant acc = new Accountant();
+		emp = acc;
+		
+		ArrayList<Employee> employees = new ArrayList<>();
+		employees.add(new Employee());
+		ArrayList<Accountant> accountants = new ArrayList<>();
+		accountants.add(new Accountant());
+		//employees = accountants;
+		
+		ArrayList<?> employees2 = new ArrayList<>();
+		ArrayList<Accountant> accountants2 = new ArrayList<>();
+		employees2 = accountants2;
+		
+		//Upper bound:
+		ArrayList<? extends Employee> employees3 = new ArrayList<>();
+		ArrayList<Accountant> accountants3 = new ArrayList<>();
+		employees3 = accountants3;
+		
+		//Lower bound:
+		ArrayList<? super Employee> employees4 = new ArrayList<>();
+		ArrayList<Object> accountants4 = new ArrayList<>();
+		employees4 = accountants4;
+		
+		System.out.println("--------------");
+		makeEmployeeWork(accountants);
+		
 	}
 	
 	public static <E> Set<E> union(Set<E> set1, Set<E> set2) {
@@ -46,6 +75,12 @@ public class Application {
 		result.addAll(set2);
 		
 		return result;
+	}
+	
+	public static void makeEmployeeWork(List<? extends Employee> employees) {
+		for (Accountant emp: (ArrayList<Accountant>) employees) {
+			emp.work();
+		}
 	}
 	
 	
