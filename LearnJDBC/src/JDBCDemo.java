88208f39c959e570708c3d028d6a78f079395065
Peter <yancong.li@alumni.ubc.ca>
2018-04-21 18:51:06 -0700

    Using JDBC
    Inserting, Updating and Deleting Data using JDBC

--- LearnJDBC/src/JDBCDemo.java
+++ LearnJDBC/src/JDBCDemo.java
@@ -9,6 +9,7 @@ public class JDBCDemo {
 	public static void main(String[] args) {
 
 		String url = "jdbc:mysql://127.0.0.1:3306/employees_database";
+		int rowsAffected = 0;
 		try {
 			// Establish Connection Object
 			Connection conn = DriverManager.getConnection(url, "root", "mysqlpassword");
@@ -17,22 +18,30 @@ public class JDBCDemo {
 			Statement statement = conn.createStatement();
 
 			// Execute the statement object
-			ResultSet resultSet = statement.executeQuery("select * from employees_tbl");
+			// rowsAffected = statement.executeUpdate(
+			// "insert into employees_tbl(id, name, dept, salary) values (900, 'Robert',
+			// 'Sales', 4000);");
+			// System.out.println("Execute insert statement - Rows Affected: " +
+			// rowsAffected);
 
+			rowsAffected = statement.executeUpdate("update employees_tbl set salary = 6000 where id = 600;");
+			System.out.println("Execute update statement - Rows Affected: " + rowsAffected);
+
+			ResultSet resultSet = statement.executeQuery("select * from employees_tbl");
 			// Process the result
 
-			// while (resultSet.next()) {
-			// System.out.println(resultSet.getString("name"));
-			// }
-
-			int salaryTotal = 0;
 			while (resultSet.next()) {
-				salaryTotal += resultSet.getInt("salary");
+				System.out.println(resultSet.getString("name") + "'s monthly salary: " + resultSet.getInt("salary"));
 			}
-			System.out.println("Total salary in the employees_tbl is: " + salaryTotal);
+
+			// int salaryTotal = 0;
+			// while (resultSet.next()) {
+			// salaryTotal += resultSet.getInt("salary");
+			// }
+			// System.out.println("Total salary in the employees_tbl is: " + salaryTotal);
 
 		} catch (SQLException e) {
-			e.printStackTrace();
+			System.out.println("Error while/update insertion");
 		}
 
 	}
